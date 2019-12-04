6e0bc02b852924195126d7830c37b9ac385b295a
joancliment <45012580+joancliment@users.noreply.github.com>
2019-12-04 16:23:45 +0100

    Update TestClock.java

--- Berlin-Uhr/src/test/TestClock.java
+++ Berlin-Uhr/src/test/TestClock.java
@@ -7,13 +7,13 @@ import main.ClockBerlin;
 public class TestClock {
 	
 	@Test
-    public void Suma_dos_numeros_calculadora () {
+    	public void Pasar_Hora_a_Berlin_Uhr () {
 		ClockBerlin time = new ClockBerlin();
-	     String timeEnt = "13:17:01";
-	     String actual = time.ConvertTime(timeEnt);
-         String expected = "O\nRROO\nRRRO\nYYROOOOOOOO\nYYOO\n";
-         Assert.assertEquals(actual, expected);
+	     	String timeEnt = "13:17:01";
+	     	String actual = time.ConvertTime(timeEnt);
+         	String expected = "O\nRROO\nRRRO\nYYROOOOOOOO\nYYOO\n";
+         	Assert.assertEquals(actual, expected);
 
-    }
+    	}
 
 }
